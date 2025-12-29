14dba26b8a9675df2902b195a000f57b0000c91e
Saurabh Jadhav <saurabhjadhav1911@gmail.com>
2025-12-29 21:33:55 +0530

    Refactor hello route to return a static message

--- hellofly.py
+++ hellofly.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route("/")
+def hello():
+    return "Hello from Fly.io!"
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
